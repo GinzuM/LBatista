4d52cb5bee0614df1ae9be12b72052ed366a53ae
Raphael <rphsaraiva@gmail.com>
2022-11-30 12:39:07 -0300

    tentativa tres

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 10000
+
+})
